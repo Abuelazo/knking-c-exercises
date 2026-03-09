6f01bdb28cc1204a2bcf4d7e76d6791ca15045ec
Abuelazo <abuelon@proton.me>
2026-03-09 13:31:12 +0900

    ch02: add detailed notes on directives and main function

--- ch02/pun.c
+++ ch02/pun.c
@@ -1,13 +1,19 @@
-// Directives are basically instructions for the preprocessor.
-// The "#include" directive states that the information about C standard I/O library which is in "<stdio.h>" has to be included in the program before it compiles it.
-// We need to do this because C has no built-in "read" and "write" commands. This ability is provided instead by functions in the standard library.
+// Directives are instructions for the preprocessor.
+// The "#include" directive states that information about the C standard I/O library (<stdio.h>) must be included before compilation.
+// This is necessary because C has no built-in "read" and "write" commands; this ability is provided by the standard library instead.
 #include <stdio.h>
 
-// Functions in C are a series of statements for the program that have been grouped together and given a name. 
-// A function that computes a value uses the return statement to specify what value it "returns"
+// Functions in C are a series of statements grouped together and given a name. 
+// A function that computes a value uses the "return" statement to specify what value it sends back.
 
+// The word "int" indicates that main returns an integer value.
+// The word "void" indicates that the function takes no arguments.
 int main(void)
 {
-	printf("To C or not to C: that is the question.\n");
-	return 0;
+    printf("To C or not to C: that is the question.\n");
+
+    // The statement "return 0" has two consequences:
+    // 1. It makes the main function terminate.
+    // 2. It indicates that the program finished successfully (value of 0).
+    return 0;
 }

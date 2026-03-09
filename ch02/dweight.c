ceda25399d382e80bb184c900e708abb142fec2d
Abuelazo <abuelon@proton.me>
2026-03-09 23:31:35 +0900

    ch02: complete dweight program

---
+++ ch02/dweight.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Compute the dimensional weight of a 12 x 10 x 8 box
+
+int main(void)
+{
+	int height, length, width, weight, volume;
+
+	height = 8;
+	length = 12;
+	width = 10;
+
+	volume = height * length * width;
+
+	// Because the shipping company expect us to round up, one solution is to add 165 to the volume before dividing by 166.
+	weight = (volume + 165) / 166;
+
+	printf("Dimensions: %dx%dx%d\n", weight, length, width);
+	printf("Volume: %d\n", volume);
+	printf("Dimensional Weight: %d\n", weight);
+}

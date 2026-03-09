ff4560ad81a42d6c7f14ed49641d3b159c660e33
Abuelazo <abuelon@proton.me>
2026-03-09 15:53:03 +0900

    ch02: fix comments

--- ch02/pun_split.c
+++ ch02/pun_split.c
@@ -3,9 +3,14 @@
 int main(void)
 {
 
-	// The function call printf does not automatically advance to the next output line when it finishes. To make printf advance one line, \n must be included in the string to be printed.
+	// The function call printf does not automatically advance to the next output line when it finishes. 
+	// To make printf advance one line, \n must be included in the string to be printed.
+	
 	printf("To C, or not to C: ");
 	printf("that is the question.\n");
+
+	// The new-line character can appear more than once in a string literal.
+	
 	printf("\n");
 	printf("Brevity is the soul of wit.\n --Shakespeare\n");
 	return 0;

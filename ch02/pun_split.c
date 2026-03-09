719baf5640b92979bd9ff2bba02c47af6c877b72
Abuelazo <abuelon@proton.me>
2026-03-09 13:49:14 +0900

    ch02: add notes on new-line character and printf

---
+++ ch02/pun_split.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	// The function call printf does not automatically advance to the next output line when it finishes. To make printf advance one line, \n must be included in the string to be printed.
+	printf("To C, or not to C: ");
+	printf("that is the question.\n");
+	printf("\n");
+	printf("Brevity is the soul of wit.\n --Shakespeare\n");
+	return 0;
+
+}

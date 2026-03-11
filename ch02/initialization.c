929b436371da4daebeb80d37d92b59c29f00fb9c
Abuelazo <abuelon@proton.me>
2026-03-11 16:55:48 +0900

    ch02: implement initialization

---
+++ ch02/initialization.c
@@ -0,0 +1,15 @@
+int main(void)
+{
+	// A variable that does not have a default value and has not been assigned one is said to be uninitialized.
+	
+	// We can initialize and declare a variable in a single line.
+	// The value of 8 is said to be an initializer.
+	// int height = 8;
+	
+	// Any number of variables can be initialized in the same declaration.
+	// int height = 8, length = 12, width = 10;
+	
+	// Every variable needs it is own initializer.
+	// 10 is only good for width, not for height or length.
+	// int height, lenght, width = 10;
+}

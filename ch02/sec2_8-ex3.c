2be5c72473633b88732e9e74ec979471eb90fdb2
Abuelazo <abuelon@proton.me>
2026-03-17 15:42:09 +0900

    ch02: complete sec 2.8 ex 3 - prompt user to enter the radius of the sphere

---
+++ ch02/sec2_8-ex3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+int main(void)
+{
+	int radius; 
+
+	printf("Enter the radius of the sphere: ");
+	scanf("%d", &radius);
+
+	// v = (4/3)πr^3
+	// 4.0f/3.0f used to avoid integer division
+	printf("Volume of the sphere: %.2f\n",
+	4.0f/3.0f * PI * radius * radius * radius);
+}
+

7864be608deeec01e6daca1220ea85372ad68198
Abuelazo <abuelon@proton.me>
2026-03-20 21:01:47 +0900

    ch03: modify addfrac.c to allow one-line input

--- ch03/addfrac.c
+++ ch03/addfrac.c
@@ -4,13 +4,9 @@ int main(void)
 {
 	int num1, denom1, num2, denom2;
 
-	printf("Enter first fraction: ");
+	printf("Enter two fractions separated by a plus sign: ");
 
-	// The space in the scanf function indicates that until it finds data it will skip all spaces/tabs/enters.	
-	scanf("%d /%d", &num1, &denom1);
-
-	printf("Enter second fraction: ");
-	scanf("%d /%d", &num2, &denom2);
+	scanf("%d /%d +%d /%d", &num1, &denom1, &num2, &denom2);
 
 	int result_num = num1 * denom2 + num2 * denom1;
 	int result_denom = denom1 * denom2;

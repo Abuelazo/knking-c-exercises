2d8df87b67c0a0e042ce4c0c553453dedf12d361
Abuelazo <abuelon@proton.me>
2026-04-04 16:56:00 +0900

    ch05: implement equality operators

---
+++ ch05/equality.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+	// In C, equality operators are == (equal to) and != (not equal to).
+	// Like the equality operators are left associative and produce either 0 (false) or 1 (true) as their result.
+	
+	int i = 2, j = 2;
+	i == j; // yields 1
+	
+	return 0;
+}

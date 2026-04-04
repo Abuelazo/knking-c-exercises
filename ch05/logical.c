bf2462fe46a3928e515c49c1f74674f5eeac5326
Abuelazo <abuelon@proton.me>
2026-04-04 16:49:29 +0900

    ch05: add simple example

---
+++ ch05/logical.c
@@ -0,0 +1,13 @@
+
+int main(void)
+
+	// Many statements in C, including if statement, must test the value of an expression to see if it is true or false.
+	// An expression such as i < j would have a special boolean or logical type in other programming languages. Such a type would have only two values, false and true.
+	// In C, however that comparison yields an integer: either 0 (false) or 1 (true).
+{
+	int i = 2, j = 3;
+
+	i < j; // yields 1 (true)
+	       
+	return 0;
+}

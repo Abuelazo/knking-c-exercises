3e18dd418b02a2197222bc590ed2e1824bd4c98d
Abuelazo <abuelon@proton.me>
2026-04-22 19:52:28 +0900

    ch05: complete date.c project

---
+++ ch05/date.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(void) {
+  int month, day, year;
+
+  printf("Enter date (mm/dd/yy): ");
+  scanf("%d/%d/%d", &month, &day, &year);
+
+  printf("Dated this %d", day);
+
+  switch (day) {
+  case 1: case 11: case 21: case 31:
+    printf("st");
+  
+  case 2: case 12: case 22:
+    printf("nd");
+  case 3: case 13: case 23:
+    printf("rd");
+  
+  default: printf("th day ");
+  }
+
+  switch (month) {
+    case 1:
+	    printf("of January");	break;
+    case 2:
+	    printf("of February"); break;
+    case 3:
+	    printf("of March"); break;
+    case 4:
+	    printf("of April"); break;
+    case 5:
+	    printf("of May"); break;
+    case 6:
+	    printf("of June"); break;
+    case 7:
+	    printf("of July"); break;
+    case 8:
+	    printf("of August"); break;
+    case 9:
+	    printf("of September"); break;
+    case 10:
+	    printf("of October"); break;
+    case 11:
+	    printf("of November"); break;
+    case 12:
+	    printf("of December"); break;
+  }
+
+  printf(", 20%d.", year);
+}

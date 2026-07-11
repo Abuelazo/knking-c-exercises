239594b630e56fc9ea2e7fa6591b52e38cab6f75
Abuelo <abuelon@proton.me>
2026-07-11 19:59:44 +0900

    ch06: implement numdigits.c program

---
+++ ch06/numdigits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int digits = 0, num;
+  printf("Enter a nonnegative integer: ");
+  scanf("%d", &num);
+
+  do {
+    num /= 10;
+    digits++;
+  } while (num != 0);
+
+  printf("The number has %d digit(s).\n", digits);
+
+  return 0;
+}

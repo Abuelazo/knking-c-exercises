a8d04ed5414798db8bd75ff30799e337b8640969
Abuelo <abuelon@proton.me>
2026-07-13 23:09:57 +0900

    ch06-proj06: add even square calculator

---
+++ ch06/project06.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, i = 2;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+
+	while (i * i <= n) {
+		printf("%d\n", i * i);
+		i += 2;
+	}
+
+	return 0;
+}
